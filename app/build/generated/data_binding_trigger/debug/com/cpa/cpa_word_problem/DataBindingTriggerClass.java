88e29d4a74a83b4d41e699381f926436ae9270c7
ssuby.shin <sys09270883@gmail.com>
2022-03-02 20:22:39 +0900

    Chore: Clear git cache

--- app/build/generated/data_binding_trigger/debug/com/cpa/cpa_word_problem/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.cpa.cpa_word_problem;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/generated/source/kapt/debug/androidx/databinding/DataBinderMapperImpl.java
+++
@@ -1,7 +0,0 @@
-package androidx.databinding;
-
-public class DataBinderMapperImpl extends MergedDataBinderMapper {
-  DataBinderMapperImpl() {
-    addMapper(new com.cpa.cpa_word_problem.DataBinderMapperImpl());
-  }
-}
