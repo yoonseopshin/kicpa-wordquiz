8069e1a8fc3759adbb17f9f69462abb30927e71a
Yoonseop <sys09270883@gmail.com>
2022-06-03 21:11:54 +0900

    홈탭 퀴즈 카드 플렉스 박스 적용
      - 굳이 리사이클러뷰로 쓸 이유는 없어 보여서 일단 레이아웃만 변경함

--- feature-quiz/build.gradle.kts
+++ feature-quiz/build.gradle.kts
@@ -16,5 +16,5 @@ dependencies {
     implementation("com.google.android.play:core-ktx:1.8.1")
 
     implementation("androidx.recyclerview:recyclerview:1.3.0-alpha02")
-
+    implementation("com.google.android.flexbox:flexbox:3.0.0")
 }
