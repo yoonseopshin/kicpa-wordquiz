54aa19689a75d26045ccb9a88326fd35acaaba1c
Yoonseop <sys09270883@gmail.com>
2022-07-01 00:29:33 +0900

    Remove flipper

--- app/src/main/kotlin/com/cpa/cpa_word_problem/CpaQuizApp.kt
+++ app/src/main/kotlin/com/cpa/cpa_word_problem/CpaQuizApp.kt
@@ -1,7 +1,6 @@
 package com.cpa.cpa_word_problem
 
 import android.app.Application
-import com.ysshin.cpaquiz.data.network.initializer.Flipper
 import com.ysshin.cpaquiz.feature.sync.initializer.Sync
 import com.ysshin.cpaquiz.shared.android.initializer.Timber
 import dagger.hilt.android.HiltAndroidApp
@@ -12,7 +11,6 @@ class CpaQuizApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Sync.initialize(context = this)
-        Flipper.initialize(context = this)
         Timber.initialize(context = this)
     }
 }
