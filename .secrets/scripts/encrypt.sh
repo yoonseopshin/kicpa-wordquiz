6e2f504e65a738820aef812828cbacb36dd76db9
Yoonseop <sys09270883@gmail.com>
2022-07-03 13:10:51 +0900

    Run CI to develop branch PR

--- .secrets/scripts/encrypt.sh
+++ .secrets/scripts/encrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p .secrets
-tar cvf .secrets/secrets.tar app/google-services.json secrets.properties
+tar cvf .secrets/secrets.tar app/google-services.json secrets.properties app/keystore/cpawordproblem.jks
 gpg -c .secrets/secrets.tar
