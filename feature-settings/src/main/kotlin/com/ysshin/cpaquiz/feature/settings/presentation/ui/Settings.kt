c203371b13425a96a46843dc24d7d317f2b9c66b
Yoonseop <sys09270883@gmail.com>
2022-07-31 01:45:28 +0900

    Change to LargeTopAppBar

--- feature-settings/src/main/kotlin/com/ysshin/cpaquiz/feature/settings/presentation/ui/Settings.kt
+++ feature-settings/src/main/kotlin/com/ysshin/cpaquiz/feature/settings/presentation/ui/Settings.kt
@@ -14,9 +14,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -69,7 +69,7 @@ fun SettingsScreen(viewModel: SettingsViewModel = viewModel()) {
 
         Scaffold(
             topBar = {
-                SmallTopAppBar(
+                LargeTopAppBar(
                     title = {
                         Text(
                             text = stringResource(id = R.string.settings),
