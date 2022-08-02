32537efb164fdcc82d00e872774527c7e7efa390
Yoonseop <sys09270883@gmail.com>
2022-08-02 22:03:12 +0900

    Enable snackbar in settings screen

--- feature-settings/src/main/kotlin/com/ysshin/cpaquiz/feature/settings/presentation/ui/Settings.kt
+++ feature-settings/src/main/kotlin/com/ysshin/cpaquiz/feature/settings/presentation/ui/Settings.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -68,6 +69,7 @@ fun SettingsScreen(viewModel: SettingsViewModel = hiltViewModel()) {
         }
 
         Scaffold(
+            snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
             topBar = {
                 LargeTopAppBar(
                     title = {
