8278af7905b9c888aa57d904133d006128ca2ee1
Yoonseop <sys09270883@gmail.com>
2022-08-02 22:47:36 +0900

    Fix lint

--- shared-android/src/main/kotlin/com/ysshin/cpaquiz/shared/android/ui/theme/Theme.kt
+++ shared-android/src/main/kotlin/com/ysshin/cpaquiz/shared/android/ui/theme/Theme.kt
@@ -82,10 +82,8 @@ fun CpaQuizTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
-    val supportDynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-
     val colorScheme = when {
-        dynamicColor && supportDynamicColor -> {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
