ae1f6abb49654973fb0d7e1af544a44dd7d7f959
Yoonseop <sys09270883@gmail.com>
2023-04-07 20:28:35 +0900

    feat: Add CpaQuizFcmService for handling Firebase Cloud Messaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,6 +118,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.messaging)
 
     debugImplementation(libs.leakcanary)
 }
---
+++ app/src/main/kotlin/com/ysshin/cpaquiz/messaging/CpaQuizFcmService.kt
@@ -0,0 +1,21 @@
+package com.ysshin.cpaquiz.messaging
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import timber.log.Timber
+
+class CpaQuizFcmService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        // Send token to server
+        Timber.d("token: $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        // Handle received message
+        Timber.d("message notification title: ${message.notification?.title}")
+        Timber.d("message notification body: ${message.notification?.body}")
+        Timber.d("message data: ${message.data}")
+    }
+}
