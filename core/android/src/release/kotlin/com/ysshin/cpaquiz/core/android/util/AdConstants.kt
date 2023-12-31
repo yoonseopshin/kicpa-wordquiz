6f92f54b23904f5b07de075fff4516db6b398cec
Yoonseop <sys09270883@gmail.com>
2023-12-31 15:16:34 +0900

    fix: Set marketRelease true

--- core/android/src/release/kotlin/com/ysshin/cpaquiz/core/android/util/AdConstants.kt
+++ core/android/src/release/kotlin/com/ysshin/cpaquiz/core/android/util/AdConstants.kt
@@ -8,7 +8,7 @@ object AdConstants {
 
     // FIXME: How do I check if app has been downloaded from the Google Play Store?
     // Checking with hardcoded variables for now
-    private val isMarketRelease = false
+    private val isMarketRelease = true
 
     fun getNativeMediumAdUnitId() = when (isMarketRelease) {
         true -> QUIZ_NATIVE_AD_MEDIUM
